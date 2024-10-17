e97040b5703858c9ba37c4906f9097b31baf237b
John-Camargo <142925070+John-Camargo@users.noreply.github.com>
2024-10-17 15:54:18 -0600

    fixing apps

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
